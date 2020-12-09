3cb3e6135335223664b6328cb127fa77c4cfa3fa
Gareth Jones <gareth@garethpaul.com>
2020-12-08 20:17:52 -0800

    add commit

---
+++ test.js
@@ -0,0 +1,20 @@
+var accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
+var authToken = process.env.TWILIO_AUTH_TOKEN;   // Your Auth Token from www.twilio.com/console
+
+const client = require('twilio')(accountSid, authToken)
+client.logLevel = 'debug'
+
+// 
+let to = '+14242059482';
+let from = '+12135237830';
+
+// Send message using promise
+var promise = client.messages.create({
+    from: from,
+    to: to,
+    body: 'create using promises'
+  });
+  promise.then(function(message) {
+    console.log('Created message using promises');
+    console.log(message.sid);
+  });
---
+++ test.py
@@ -0,0 +1,23 @@
+import os
+import logging
+
+from twilio.rest import Client
+
+class CompanyComms:
+
+    def sendMsg(self, to_number, from_number, body):
+        """Run basic send message"""
+
+        client = Client(os.getenv('TWILIO_ACCOUNT_SID'), os.getenv('TWILIO_AUTH_TOKEN'))
+        #logging.basicConfig()
+        #client.http_client.logger.setLevel(logging.INFO)
+
+        message = client.messages.create(to=to_number,
+                                        from_=from_number,
+                                        body=body)
+        return message
+
+
+if __name__ == "__main__":
+    c = CompanyComms()
+    c.sendMsg('+14242059482', '+12135236830', 'hello')
